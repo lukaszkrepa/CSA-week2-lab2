827b984ddbcd9cab56e17910979e689bd76e1399
LukaszKrepa <lukaszkrepa@yahoo.com>
2024-09-26 15:44:17 +0100

    Finished

--- client/client.go
+++ client/client.go
@@ -2,38 +2,40 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"net"
 	"os"
 )
 
 func read(conn net.Conn) {
-	reader := bufio.NewReader(conn)
-	msg, _ := reader.ReadString('\n')
-	fmt.Println(msg)
-
+	for {
+		reader := bufio.NewReader(conn)
+		msg, _ := reader.ReadString('\n')
+		fmt.Println("Message Received: ", msg)
+	}
 }
 
 func write(conn net.Conn) {
-	//TODO Continually get input from the user and send messages to the server.
-}
-
-func main() {
-
-	//// Get the server address and port from the commandline arguments.
-	//addrPtr := flag.String("ip", "127.0.0.1:8030", "IP:port string to connect to")
-	//flag.Parse()
-	////TODO Try to connect to the server
-	////TODO Start asynchronously reading and displaying messages
-	////TODO Start getting and sending user messages.
-
 	stdin := bufio.NewReader(os.Stdin)
-	conn, _ := net.Dial("tcp", "127.0.0.1:8030")
-
 	for {
 		fmt.Printf("Enter text->")
 		msg, _ := stdin.ReadString('\n')
 		fmt.Fprintln(conn, msg)
-		read(conn)
 	}
+}
+
+func main() {
+
+	// Get the server address and port from the commandline arguments.
+	addrPtr := flag.String("ip", "127.0.0.1:8030", "IP:port string to connect to")
+	flag.Parse()
+	//TODO Try to connect to the server
+	//TODO Start asynchronously reading and displaying messages
+	//TODO Start getting and sending user messages.
+
+	conn, _ := net.Dial("tcp", *addrPtr)
+	go read(conn)
+	go write(conn)
+	select {}
 }
--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"net"
 )
@@ -17,17 +18,19 @@ func handleError(err error) {
 }
 
 func acceptConns(ln net.Listener, conns chan net.Conn) {
-	// TODO: all
-	// Continuously accept a network connection from the Listener
-	// and add it to the channel for handling connections.
+	for {
+		conn, _ := ln.Accept()
+		conns <- conn
+	}
+
 }
 
 func handleClient(client net.Conn, clientid int, msgs chan Message) {
-	// TODO: all
-	// So long as this connection is alive:
-	// Read in new messages as delimited by '\n's
-	// Tidy up each message and add it to the messages channel,
-	// recording which client it came from.
+	reader := bufio.NewReader(client)
+	for {
+		msg, _ := reader.ReadString('\n')
+		msgs <- Message{message: msg, sender: clientid}
+	}
 }
 
 func handleConnection(conn net.Conn) {
@@ -39,37 +42,33 @@ func handleConnection(conn net.Conn) {
 	}
 }
 func main() {
-	// Read in the network port we should listen on, from the commandline argument.
-	// Default to port 8030
-	//portPtr := flag.String("port", ":8030", "port to listen on")
-	//flag.Parse()
+	//Read in the network port we should listen on, from the commandline argument.
+	//Default to port 8030
+	portPtr := flag.String("port", ":8030", "port to listen on")
+	flag.Parse()
 
-	//TODO Create a Listener for TCP connections on the port given above.
-	ln, _ := net.Listen("tcp", ":8030")
+	ln, _ := net.Listen("tcp", *portPtr)
+	//Create a channel for connections
+	conns := make(chan net.Conn)
+	//Create a channel for messages
+	msgs := make(chan Message)
+	//Create a mapping of IDs to connections
+	clients := make(map[int]net.Conn)
+	id := 0
+	//Start accepting connections
+	go acceptConns(ln, conns)
 	for {
-		conn, _ := ln.Accept()
-		go handleConnection(conn)
+		select {
+		case conn := <-conns:
+			clients[id] = conn
+			go handleClient(conn, id, msgs)
+			id++
+		case msg := <-msgs:
+			for i := 0; i < id; i++ {
+				if !(msg.sender == i) {
+					fmt.Fprintln(clients[i], msg.message)
+				}
+			}
+		}
 	}
-
-	////Create a channel for connections
-	//conns := make(chan net.Conn)
-	////Create a channel for messages
-	//msgs := make(chan Message)
-	////Create a mapping of IDs to connections
-	//clients := make(map[int]net.Conn)
-	//
-	////Start accepting connections
-	//go acceptConns(ln, conns)
-	//for {
-	//	select {
-	//	case conn := <-conns:
-	//		//TODO Deal with a new connection
-	//		// - assign a client ID
-	//		// - add the client to the clients map
-	//		// - start to asynchronously handle messages from this client
-	//	case msg := <-msgs:
-	//		//TODO Deal with a new message
-	//		// Send the message to all clients that aren't the sender
-	//	}
-	//}
 }
